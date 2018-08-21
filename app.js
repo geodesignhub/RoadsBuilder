1eba5b284ccfbe1dba835ddc8614b3c5d78d64cc
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2018-08-21 18:04:42 +0100

    Updated Base URL

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(csrf({ cookie: true }));
 app.get('/', function(request, response) {
     var opts = {};
     if (request.query.apitoken && request.query.projectid) {
-        var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
-        // var baseurl = 'http://local.dev:8000/api/v1/projects/';
+        // var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+        var baseurl = 'http://local.dev:8000/api/v1/projects/';
         var apikey = request.query.apitoken;
         var cred = "Token " + apikey;
         var projectid = request.query.projectid;
@@ -65,8 +65,8 @@ app.get('/', function(request, response) {
 
 app.post('/adddiagram/', function(request, response) {
     // post json back 
-    var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
-    // var baseurl = 'http://local.dev:8000/api/v1/projects/';
+    // var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+    var baseurl = 'http://local.dev:8000/api/v1/projects/';
     var projectid = request.body.projectid;
     var apitoken = request.body.apitoken;
     var diagname = request.body.diagname;
